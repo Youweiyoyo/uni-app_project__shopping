543e17d8063bbf1fb88a29877e42c4f299f627e1
Youwei_star <1176071733@qq.com>
2021-04-09 12:50:19 +0800

    封装商品item项 by Youwei

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,72 @@
+<template>
+	<view>
+		<view class="iemtList" >
+		  <!-- 左侧图片 -->
+		  <view class="listLeft">
+		    <image :src="goods.goods_small_logo || defaultPic" mode="widthFix"></image>
+		  </view>
+		  <!-- 右侧文字 -->
+		  <view class="rightList">
+		    <view class="text">
+		      {{goods.goods_name}}
+		    </view>
+		    <view class="Price">
+		      ￥{{goods.goods_price | tofixed}}
+		    </view>
+		  </view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+    props:{
+      goods:{
+        type:Object,
+        default:{}
+      }
+    },
+    filters:{
+      tofixed(num){
+        return Number(num).toFixed(2)
+      }
+    },
+    onLoad(){
+    },
+		data() {
+			return {
+				// 默认图片
+				defaultPic:'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.iemtList{
+    display: flex;
+    padding: 5px 0;
+    border-bottom: 1px solid #f0f0f0;
+    .rightList{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .text{
+        font-size: 13px;
+      }
+      .Price{
+        font-size: 16px;
+        color: #c00000;
+      }
+    }
+    .listLeft{
+      image {
+        width: 100px;
+        height: 100px;
+        display: block;
+        margin-right: 10px;
+      }
+    }
+  }
+</style>
--- subpkg/good_list/good_list.vue
+++ subpkg/good_list/good_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		<text>我是商品列表</text>
+		<view class="goodList">
+		  <block v-for="(item,index) in goodLsit" :key="index" class="block">
+        <my-goods :goods="item"></my-goods>
+      </block>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +12,36 @@
 	export default {
 		data() {
 			return {
-				
+        // 需要传递的商品列表数据
+				queryObj:{
+          query:'',        // 查询关键词
+          cid:'' ,        // 查询商品的id
+          pagenum:'',    // 页码值
+          pagesize:''  // 每页显示多少条数据
+        },
+        goodLsit:[] , // 存total:0 放分页商品列表数据
+        total:0 , // 数据条数，用来实现分页
 			};
-		}
+		},
+    onLoad(option){
+      this.queryObj.query = option.query || '' // ||遇到假就往后走，遇到真或者走到最后就返回当前的值
+      this.queryObj.cid = option.cid || ''
+      this.getGoodList()   // 调用获取商品列表的数据
+    },
+    methods:{
+      async getGoodList(){
+       const {data:res} = await uni.$http.get('/api/public/v1/goods/search',this.queryObj)
+       console.log(res)
+       if(res.meta.status !== 200) return uni.$showMsg('获取数据失败')
+       this.goodLsit = res.message.goods
+       this.total = res.message.total
+      }
+    }
 	}
 </script>
 
 <style lang="scss">
-
+.goodList{
+  padding: 10px;
+}
 </style>
